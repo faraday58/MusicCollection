fdc28054af30d5aedce4f7f7292c71ffee19f61b
Armando <jarodriguezvera@gmail.com>
2023-08-07 09:33:32 -0600

    Initial Commit

---
+++ MusicCollection/MusicCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicCollectionApp.swift
+//  MusicCollection
+//
+//  Created by Armando Rodríguez on 07/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
